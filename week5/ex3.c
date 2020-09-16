692ff0f287d0fb430d0a05ba3c65e36e7de90887
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-09-16 16:57:20 +0300

    task3 done

--- week5/ex3.c
+++ week5/ex3.c
@@ -2,75 +2,73 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define BUFFER_SIZE 1
+// In average it takes from 1000 to 25000 iterations
+// to fatal race condition will appear 
+
+
+#define BUFFER_SIZE 100
+
+int buffer[BUFFER_SIZE];
+int counter = 0;
 int producer_sleep = 0;
 int consumer_sleep = 1;
 
-void consumer_job(int *buffer){
+void consumer_job(){
     while (1){
-        if (!consumer_sleep){
-            printf("Consumer: start consuming. First value of buffer is %d\n", buffer[0]);
-            if (buffer[0] != 1){
-                printf("Consumer: press F to pay respect");
-                fflush(stdout);
-            }
-            fflush(stdout);
-            
-            for (int i = 0; i < BUFFER_SIZE; i++){
-                buffer[i] = 0;
-            }
-            
-            printf("Consumer: consumed buffer!\n");
-            consumer_sleep = 1;
+
+        // Busy waiting
+        while (consumer_sleep) {}
+
+        if (counter > 0){
+            counter--;
             producer_sleep = 0;
-            fflush(stdout);
+            buffer[counter] = 0;
         }
         else{
-            printf("Consumer: Sleeeeeeeeeeep...\n");
-            sleep(1);
-            fflush(stdout);
+            // if counter is 0, then we need to sleep
+            consumer_sleep = 1;
         }
+
     }
 }
 
-void producer_job(int *buffer){
+void producer_job(){
+    int i = 0;
     while (1){
-        if (!producer_sleep){
-            printf("Producer: start producing. First value of buffer is %d\n", buffer[0]);
+        
+        // busy waiting
+        while (producer_sleep){}
+
+        if (i % 100 == 0) {
+            printf("%d\n", i);
             fflush(stdout);
-            if (buffer[0] != 0){
-                printf("Producer: press F to pay respect");
-                fflush(stdout);
-            }
-            for (int i = 0; i < BUFFER_SIZE; i++){
-                buffer[i] = 1;
-            }
-            
-            printf("Producer: produced buffer!\n");
+        }
+        
+        if (counter < BUFFER_SIZE){
             consumer_sleep = 0;
-            producer_sleep = 1;
-            fflush(stdout);
+            buffer[counter] = i;
+            counter++;
+
         }
         else{
-            printf("Producer: Sleeeeeeeeeeep...\n");
-            sleep(1);
-            fflush(stdout);
+            // If counter == Buffer Size, then sleep
+            producer_sleep = 1;
         }
+        i++;
+        
     }
 }
 
 
 int main(int argc, char const *argv[]){
-    
-    int *buffer = malloc(BUFFER_SIZE);
 
     pthread_t producer, consumer;
     printf("Start producer\n");
-    pthread_create(&producer, NULL, producer_job, buffer);
+    pthread_create(&producer, NULL, producer_job, NULL);
     
     printf("Start consumer\n");
-    pthread_create(&consumer, NULL, consumer_job, buffer);
-    pthread_join(consumer, NULL);
+    pthread_create(&consumer, NULL, consumer_job, NULL);
+    pthread_join(producer, NULL);
 
     return 0;
 }
