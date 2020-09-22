b444bfc73e3f7a545c8d8fc55c4eaaed1610f3a9
Lev Lymarenko <lymarenko.lev@gmail.com>
2020-09-22 14:04:24 +0300

    rewrite task 1

--- week6/ex1.c
+++ week6/ex1.c
@@ -23,6 +23,19 @@ void buble_sort(int** array, int size){
     
 }
 
+int first_come_first_served(int **array, int *processed, int size){
+    // Returns index of next process to be execute
+
+    for (int i = 0; i < size; i++){
+        if (!processed[i]){
+            processed[i] = 1;
+            return i;
+        }
+    }
+    return size;
+    
+}
+
 
 int main(int argc, char const *argv[]){
     FILE *fptr;
@@ -32,23 +45,25 @@ int main(int argc, char const *argv[]){
     fscanf(fptr,"%d", &N);
     
     int **array = malloc(N * sizeof(int*));
-    
 
-    int max_arravial_time = 0;
     for (int i = 0; i < N; i++){
         array[i] = malloc(2 * sizeof(int));
         fscanf(fptr, "%d %d", &array[i][0], &array[i][1]);
 
     }
+    
     buble_sort(array, N);
 
     int t = 0;
-    
     int turnaround_time_sum = 0;
     int waiting_time_sum = 0;
+    int *processed = malloc(N * sizeof(int));
+
     for (int i = 0; i < N; i++){
-        int arravial_time = array[i][0];
-        int burst_time = array[i][1];
+        int next_process_index = first_come_first_served(array, processed, N);
+        int arravial_time = array[next_process_index][0];
+        int burst_time = array[next_process_index][1];
+        
         t += burst_time;
         int turnaround_time = t - arravial_time;
         int waiting_time = turnaround_time - burst_time;
@@ -56,7 +71,7 @@ int main(int argc, char const *argv[]){
         turnaround_time_sum += turnaround_time;
         waiting_time_sum += waiting_time;
         
-        printf("P%d:\tExit time: %d\tTurnaround time:%d\tWaiting time:%d\n", i, t, turnaround_time, waiting_time);
+        printf("P%d(%d, %d):\tExit time: %d\tTurnaround time:%d\tWaiting time:%d\n", i, arravial_time, burst_time, t, turnaround_time, waiting_time);
         
     }
 
