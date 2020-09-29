d8f2bda36207a592aa729a2ec372f21893556420
sevenzing <41516657+sevenzing@users.noreply.github.com>
2020-09-29 15:19:26 +0300

    %s

--- week7/ex5.c
+++ week7/ex5.c
@@ -6,7 +6,7 @@ int main() {
     char **s = malloc(sizeof(char *));
     char foo[] = "Hello World";
     *s = foo;
-    printf("s is %s\n", s); // should it be %p instead of %p
+    printf("s is %s\n", s); // should it be %p instead of %s
     s[0] = foo;
     printf("s[0] is %s\n",s[0]);
     return(0);
